f71633fd17b07a3937f5624408c888e619f052fa
Camden McKenzie <camden.mckenzie@outlook.com>
2022-01-03 21:43:19 -0500

    Styling
    Added Navbar w. temp manus
    Added SVG background
    Added global colors

--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,38 @@
 import styles from './styles/navbar.module.css'
+import Link from 'next/link'
 
 export default function Navbar() {
   return (
-    <div className={styles.dropdown_menu_button}>TEST</div>
+    <nav role="navigation" className={styles.navbar}>
+      <ul className={styles.menu}>
+        <li className={styles.item}>
+          <Link href={`/`}>
+            <a>Home</a>
+          </Link>
+        </li>
+        <li className={styles.item}>
+          <Link href={`/about`}>
+            <a>About</a>
+          </Link>
+        </li>
+        <li className={styles.item}>
+          <Link href={`/`}>
+            <a>Python</a>
+          </Link>
+          <ul className={styles.menu}>
+            <li className={styles.item}>
+              <Link href={`/content/python/ex1`}>
+                <a>Ex 1</a>
+              </Link>
+            </li>
+            <li className={styles.item}>
+              <Link href={`/content/python/ex2`}>
+                <a>Ex 2</a>
+              </Link>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
   )
 }
